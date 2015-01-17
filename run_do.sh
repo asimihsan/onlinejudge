dc5bf7b72ece8c93ea6f3be48b823a7bcefb7124
Asim Ihsan <asim.ihsan@gmail.com>
2015-01-17 14:16:34 -0800

    run_do - run on 1GB hosts

--- run_do.sh
+++ run_do.sh
@@ -26,7 +26,7 @@ IMAGE_ID=$(echo "${IMAGE}" | jq '.id')
 echo $IMAGE_ID
 curl -X POST -H 'Content-Type: application/json' \
     -H "Authorization: Bearer ${DO_TOKEN}" -d \
-    "{\"name\":\"run1.runsomecode.com\",\"region\":\"sfo1\",\"size\":\"512mb\",\"ssh_keys\":[\"610664\"],\"image\":\"${IMAGE_ID}\"}" \
+    "{\"name\":\"run1.runsomecode.com\",\"region\":\"sfo1\",\"size\":\"1g\",\"ssh_keys\":[\"610664\"],\"image\":\"${IMAGE_ID}\"}" \
     "https://api.digitalocean.com/v2/droplets" | jq '.'
 
 # List all droplets, get names and ip addresses
