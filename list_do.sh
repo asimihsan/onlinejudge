266fb541d893acacd849fc3d3955af78125596ed
Asim Ihsan <asim.ihsan@gmail.com>
2015-01-17 21:50:44 -0800

    list_do.sh - fix to only list rusomecode IP addresses

--- list_do.sh
+++ list_do.sh
@@ -3,4 +3,4 @@
 curl -X GET -H 'Content-Type: application/json' \
     -H "Authorization: Bearer ${DO_TOKEN}" \
     "https://api.digitalocean.com/v2/droplets?page=1&per_page=50" \
-    | jq '[.droplets[] | .name, .id, [.networks.v4[].ip_address]]'
+    | jq '[.droplets[] | select(.name | contains("runsomecode.com")) | .name, .id, [.networks.v4[].ip_address]]'
